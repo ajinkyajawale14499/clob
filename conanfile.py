e3359ed8f88770bf06be49d75565236b860bdbca
ajinkyajawale14499 <ajinkyajawale14499@gmail.com>
2026-05-16 15:51:51 +0100

    build: CMake + Conan 2 + Catch2 v3 with ASan/UBSan presets

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
+
+
+class ClobConan(ConanFile):
+    name = "clob"
+    version = "0.1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        # Latest stable on Conan Center as of 2026-05-16.
+        self.requires("catch2/3.7.1")
+        # W5+: rapidcheck (verified via `conan search rapidcheck/*` before pinning)
+        # W6+: quill (async logging)
+        # W10+: onnxruntime, hdrhistogram_c
+
+    def layout(self):
+        cmake_layout(self)
